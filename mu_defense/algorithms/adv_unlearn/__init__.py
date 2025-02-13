23020f2ff1c8e8657e36d64a2a3eeff606f63574
palisthadeshar <palisthadeshar@gmail.com>
2025-02-13 12:52:49 +0545

    evaluation added for mu_defense

--- mu_defense/algorithms/adv_unlearn/__init__.py
+++ mu_defense/algorithms/adv_unlearn/__init__.py
@@ -3,13 +3,15 @@ from .model import AdvUnlearnModel
 from .dataset_handler import AdvUnlearnDatasetHandler
 from .compvis_trainer import AdvUnlearnCompvisTrainer
 from .diffuser_trainer import AdvUnlearnDiffuserTrainer
+from .evaluator import MUDefenseEvaluator
 # from .algorithm import AdvUnlearnAlgorithm
 # from .trainer import AdvUnlearnTrainer
 
 __all__ = ["AdvUnlearnModel", 
            "AdvUnlearnDatasetHandler",
              "AdvUnlearnCompvisTrainer",
-             "AdvUnlearnDiffuserTrainer"
+             "AdvUnlearnDiffuserTrainer",
+             "MUDefenseEvaluator"
             #  "AdvUnlearnAlgorithm",
             #  "AdvUnlearnTrainer"
 ]
